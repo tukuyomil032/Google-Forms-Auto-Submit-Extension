32211e1f3c7ae130fb82e22d1e515ca64f9aafed
tukuyomil032 <yinkom0708@gmail.com>
2025-12-19 23:46:59 +0900

    Fix: UI/UX

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { crx, defineManifest } from '@crxjs/vite-plugin'
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: "Google Form Automator",
+  name: "N/S/R高 通学コース 出席フォーム自動提出拡張機能",
   version: "1.0.0",
   permissions: ["storage", "tabs", "scripting"],
   host_permissions: ["https://docs.google.com/forms/*"],
